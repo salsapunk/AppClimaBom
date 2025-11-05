11a00da7cb65f9c820febde9a82d5c993dc0453f
bernardo <bernardinhogamespray@gmail.com>
2025-11-05 17:29:21 -0300

    README.md atualizado

--- src/weather.py
+++ src/weather.py
@@ -1,14 +1,14 @@
 import requests
+#importar as variaveis de ambiente .env
 
 #pegas no menu
 lat = -9
 lon = -36
 
-api_key = 123
 #da pra ver quando cria um usuario no openweather api
 
 #5 dias/ 3horas
-url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}'
+url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}'
 
 #request = requests.get(url)
 
