41b6f9d6c873e4731f33018ead37c6ae52f2081c
lucarl07 <junitinho2007@gmail.com>
2025-11-12 19:04:52 -0300

    Usar estado de sessão para permitir updates na interface

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
 import streamlit as st
 
-st.title("ClimaBom")
+## Dados para exibição
+# (números são figurativos e serão alterados após a modificação destes dados)
+if 'dia_0' not in st.session_state:
+  st.session_state['dia_0'] = {
+    'temp': 28,
+    'temp_min': 24,
+    'temp_max': 29,
+    'sensacao': 31,
+    'umidade': 50, # NÃO SE PREOCUPAR NO MOMENTO; Será obtida posteriormente
+    'vento_velo_kmph': 19
+  }
 
 # Funções:
 def pesquisarCidade():
   print(cidade, estado) # <- Substituir por interação com weather.py
+
+# Título da aplicação
+st.title("ClimaBom")
 
 # Definindo linhas da aplicação:
 row1 = st.columns([3, 2.5, 1], vertical_alignment='bottom')
@@ -24,21 +37,26 @@ with st.container(key="resultados_dia_0", border=True):
   row2 = st.columns(2)
 
   with row2[0]:
-    st.header('30 °C')
-    st.markdown('**Parcial, nublado** <br> **MAX:** 29° | **MIN:** 24° <br/> Sensação Térmica de 31°', True)
+    st.header(f'{st.session_state['dia_0']['temp']} °C')
+    st.markdown(f'''
+      **Parcial, nublado** <br/>
+      **MIN:** {st.session_state['dia_0']['temp_min']}° | 
+      **MAX:** {st.session_state['dia_0']['temp_max']}° <br/>
+      Sensação Térmica de {st.session_state['dia_0']['sensacao']}°
+    ''', True)
   
   with row2[1]:
     st.subheader('Maceió, AL')
     subrow = st.columns(2)
     
     with subrow[0]:
-      st.markdown('''
+      st.markdown(f'''
         #### Umidade:
-        70%
+        {st.session_state['dia_0']['umidade']}%
       ''')
 
     with subrow[1]:
-      st.markdown('''
+      st.markdown(f'''
         #### Vento:
-        19 km/h
-      ''')
+        {st.session_state['dia_0']['vento_velo_kmph']} km/h
+    ''')
