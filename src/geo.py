dfb5d7694a45a25213738837c0cb4118595d646b
bernardo <bernardinhogamespray@gmail.com>
2025-11-13 08:52:57 -0300

    removendo arquivos inúteis

--- src/geo.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="AppClima")
-Localidade = geolocator.geocode("Maceió AL")
-# print(Localidade.address)
-# print((Localidade.latitude, Localidade.longitude))
