a4e787e64a0e197294ec2599fe1949ee13c76398
lucarl07 <junitinho2007@gmail.com>
2025-11-25 15:43:02 -0300

    Corrigir e refatorar código do front + add. a direção do vento

--- src/app_bom.py
+++ src/app_bom.py
@@ -1,8 +1,30 @@
 import streamlit as st
 from weather import Resposta
 
+# Funções:
+def pesquisarCidade():
+  r = Resposta(cidade, estado)
+  r.requisicao()
+
+  return r.d_clima
+
 # Título da aplicação
-st.title("ClimaBom")
+st.title("AppClimaBom")
+
+# Definindo variáveis de estado de sessão
+if 'pesquisa_feita' not in st.session_state:
+  st.session_state.pesquisa_feita = False
+
+if 'dia_0' not in st.session_state:
+  st.session_state.dia_0 = {
+    'temp': None,
+    'temp_min': None,
+    'temp_max': None,
+    'sensacao': None,
+    'umidade': None,
+    'vento_velo': None,
+    'vento_dir': None
+  }
 
 # Definindo linhas da aplicação:
 row1 = st.columns([3, 2.5, 1], vertical_alignment='bottom')
@@ -14,35 +36,24 @@ with row1[0]:
 with row1[1]:
   estado = st.text_input("Estado", placeholder="Ex.: São Paulo")
 
-# Funções:
-def pesquisarCidade():
-  r = Resposta(cidade, estado)
-  r.requisicao()
-
-  return r.d_clima
-
-if 'pesquisa_feita' not in st.session_state:
-  st.session_state.pesquisa_feita = False
-
 with row1[2]:
   p = None
   pesquisar = st.button('Buscar')
 
   if pesquisar:
     p = pesquisarCidade()
+    st.session_state.dia_0 = {
+      'temp': p.clima_dia1['temperatura'],
+      'temp_min': p.clima_dia1['temperatura mínima'],
+      'temp_max': p.clima_dia1['temperatura máxima'],
+      'sensacao': "N/A",
+      'umidade': "N/A", # NÃO SE PREOCUPAR NO MOMENTO; Será obtida posteriormente
+      'vento_velo': "{:.3f}".format(
+        p.clima_dia1['velocidade do vento'] / 3.6
+      ),
+      'vento_dir': p.clima_dia1['direção do vento']
+    }
     st.session_state.pesquisa_feita = True
-
-## Dados para exibição
-# (números são figurativos e serão alterados após a modificação destes dados)
-if 'dia_0' not in st.session_state:
-  st.session_state.dia_0 = {
-    'temp': p.clima_dia1['temperatura'],
-    'temp_min': p.clima_dia1['temperatura mínima'],
-    'temp_max': p.clima_dia1['temperatura máxima'],
-    #'sensacao': 31,
-    #'umidade': 50, # NÃO SE PREOCUPAR NO MOMENTO; Será obtida posteriormente
-    'vento_velo_kmph': p.clima_dia1['velocidade do vento']
-  }
 
 # 2° Linha | Resultados:
 if st.session_state.pesquisa_feita == True:
@@ -59,17 +70,18 @@ if st.session_state.pesquisa_feita == True:
       ''', True)
     
     with row2[1]:
-      st.subheader('Maceió, AL')
+      st.subheader(f'{cidade}, {estado}') # Valores temporários; serão substituído por valores obtidos da API
       subrow = st.columns(2)
-      
+
       with subrow[0]:
+        st.markdown(f'''
+          #### Vento:
+          {st.session_state['dia_0']['vento_velo']} m/s <br/>
+          Sentido {st.session_state['dia_0']['vento_dir']}
+      ''', True)
+        
+      with subrow[1]:
         st.markdown(f'''
           #### Umidade:
           {st.session_state['dia_0']['umidade']}%
         ''')
-
-      with subrow[1]:
-        st.markdown(f'''
-          #### Vento:
-          {st.session_state['dia_0']['vento_velo_kmph']} km/h
-      ''')
